5087e34a406279a1c11b34dfacf635ef97d97be2
lunghekani <lunghivlanga@gmail.com>
2023-05-05 15:58:49 +0200

    Adding to a list and basic manipulation

--- FileManipulation/Program.cs
+++ FileManipulation/Program.cs
@@ -15,23 +15,29 @@ namespace FileManipulation
             // ~ - root directory
             string filePath = "./MOCK_DATA.csv"; 
 
-
+            List<string> userInfo = new List<string>(); // creating a list to store all user info
             // StreamReader Class 
             using(StreamReader sr = new StreamReader(filePath))
             { // file is now open and ready to be read
-                
+
                 sr.ReadLine(); // reading the first line in the file
 
                 while (!sr.EndOfStream)
                 {
                     string line = sr.ReadLine();
-                    
-                    Console.WriteLine(line);
-                    
-
+                    string[] arrUserInfo = line.Split(','); // splitting into an array
+                    userInfo.Add($"Name: {arrUserInfo[1]} surname: {arrUserInfo[2]} "); 
                 }
 
             }
+
+            foreach (var item in userInfo)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
             Console.ReadLine();
             // reading from a file
 
